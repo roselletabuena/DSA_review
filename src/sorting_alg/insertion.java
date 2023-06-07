fc6b3370c23772ac10f7c47746f930310af6a543
Roselle Tabuena <roselleroxtabuena@gmail.com>
2023-06-07 21:57:08 +0800

    in progress selection_sort

---
+++ src/sorting_alg/insertion.java
@@ -0,0 +1,34 @@
+package sorting_alg;
+
+public class insertion {
+    public static void main(String[] args) {
+        int[] intArray =  {20, 35, -15, 7, 55, 1, -22};
+
+//        boolean stop_loop;
+        for (int firstUnsortedIndex = 0; firstUnsortedIndex < intArray.length - 1; firstUnsortedIndex++) {
+
+
+//            stop_loop = false;
+//            int num = 0;
+//            while (!stop_loop) {
+//
+//                if(intArray[firstUnsortedIndex] < intArray[firstUnsortedIndex + 1]) {
+//                    num = intArray[firstUnsortedIndex];
+//                    stop_loop = true;
+//                }
+//            }
+
+//            System.out.println(num);
+//            stop_loop = true;
+//            int largestElement = 0;
+//            for (int i = 0; i < intArray.length; i++) {
+//
+//                if(intArray[firstUnsortedIndex] > intArray[i + 1]) {
+//                    largestElement = intArray[i];
+//                }
+//
+//            }
+
+        }
+    }
+}
